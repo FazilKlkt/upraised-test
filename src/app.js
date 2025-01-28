a75a6aab086ab00fe713644e2db97eac9e810788
Mohammed Fazil Husen <fazil.husen6@gmail.com>
2025-01-28 13:50:43 +0530

    added cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require("cors");
 const authRoutes = require('./routes/authRoutes');
 const gadgetRoutes = require('./routes/gadgetRoutes');
 const swaggerUi = require('swagger-ui-express');
@@ -10,6 +11,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/auth', authRoutes);
 app.use('/gadgets', gadgetRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
